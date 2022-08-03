4ec88a257ce1187f26bb2d5960793baba8d8279e
99liza99 <liza.owchinnikowa@gmail.com>
2022-08-03 16:07:34 +0300

    fix

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import Doctor from "./Doctor.js";
 
 const PORT = 5000;
 const app = express();
-const DB_URL =
-  "mongodb+srv://user11:user11@cluster0.fk8v0.mongodb.net/?retryWrites=true&w=majority";
+// const DB_URL =
+//   "mongodb+srv://user11:user11@cluster0.fk8v0.mongodb.net/?retryWrites=true&w=majority";
 
 app.use(
   express.json()
@@ -22,9 +22,25 @@ app.post("/", async (req, res) => {
 
 app.post("/add-doctor", async (req, res) => {
   console.log(req.body);
-  const { name, description, age } = req.body;
-  const newDoctor = await Doctor.create({ name, description, age });
+  const { name, description, specialization, age } = req.body;
+  const newDoctor = await Doctor.create({
+    name,
+    description,
+    specialization,
+    age,
+  });
   res.status(200).json(newDoctor);
+});
+
+app.get("/get-doctors", async (request, response) => {
+  const doctors = await Doctor.find();
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  response.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  response.json(doctors);
 });
 
 async function startApp() {
